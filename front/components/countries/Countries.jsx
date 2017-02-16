bc08373d24db84243bf1465f0f11797d0b61ad01
Dee Sterling <darlenesterling@gmail.com>
2017-02-16 02:30:56 -0500

    completed api call thus far

---
+++ front/components/countries/Countries.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import $ from 'jQuery'
+
+const Countries = React.createClass({
+  getInitialState(){
+    return({
+      country: '',
+      capital: '',
+      population: '',
+    })
+  },
+
+  componentDidMount: function(){
+    $.ajax({
+      type: 'GET',
+      url: 'https://restcountries.eu/rest/v1/all',
+      success: function(data){
+        return({
+        countryName: this.state.name,
+        capitalCity: this.state.capital,
+        population: this.state.population,
+      })
+    }
+  })
+},
+
+render: function(){
+  return (
+    <div>
+      <h1>{this.state.country.name}</h1>
+      <h1>{this.state.capital.capital}</h1>
+      <h1>{this.state.population.population}</h1>
+    </div>
+    )
+  }
+})
